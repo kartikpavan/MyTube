9664efd91fb57d7aed42c6522c885077f3eb84a5
kartikpavan <kartik.varanasi@yahoo.com>
2022-07-15 00:13:53 +0530

    Navbar completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Category from "../components/Category";
+import Create from "../components/Create";
+import Feed from "../components/Feed";
+import Navbar from "../components/Navbar";
+import VideoPin from "../components/VideoPin";
 
 const Home = ({ user }) => {
 	console.log(user);
-	return <div>Home</div>;
+	return (
+		<div>
+			<Navbar user={user} />
+			<div className="flex flex-col justify-start gap-8">
+				<Category />
+			</div>
+			<div className="flex w-full justify-center items-center px-4">
+				<Routes>
+					<Route path="/" element={<Feed />} />
+					<Route path="/category/:categoryID" element={<Feed />} />
+					<Route path="/create" element={<Create />} />
+					<Route path="/videoDetail/:videoID" element={<VideoPin />} />
+				</Routes>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
