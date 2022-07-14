a23e121e29fe0f9f18113e4ac3f984868c80fdb8
kartikpavan <kartik.varanasi@yahoo.com>
2022-07-14 21:25:33 +0530

    stored user details to firestore

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,11 +5,15 @@ import { useNavigate } from "react-router-dom";
 //* google authentication
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { firebaseApp } from "../firebase";
+//* firebase firestore
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const Login = () => {
 	const provider = new GoogleAuthProvider();
 	const auth = getAuth(firebaseApp);
-	const navigate = useNavigate;
+	const db = getFirestore(firebaseApp);
+
+	const navigate = useNavigate();
 
 	const signIn = async () => {
 		const { user } = await signInWithPopup(auth, provider);
@@ -19,6 +23,10 @@ const Login = () => {
 		//* Storing the refreshTOken and the user data in browser local storage
 		localStorage.setItem("user", JSON.stringify(providerData));
 		localStorage.setItem("userToken", JSON.stringify(refreshToken));
+
+		//* sending user details to firebase firestore
+		await setDoc(doc(db, "users", providerData[0].uid), providerData[0]);
+		navigate("/", { replace: true });
 	};
 
 	return (
